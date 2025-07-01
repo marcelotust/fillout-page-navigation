f521537e82c159eb086ba4955b951be69fa80c48
Marcelo Tust <marcelotust@gmail.com>
2025-07-01 16:23:57 -0300

    name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Page Navigation Example",
+  title: "Page Navigation",
   description: "Fillout Page Navigation Challenge",
 };
 
